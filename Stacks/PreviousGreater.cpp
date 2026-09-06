da15d74139802e22c41b2f4fcb6980c978b30449
geekyraghav <49072935+geekyraghav@users.noreply.github.com>
2026-09-06 16:30:09 +0530

    Create PreviousGreater.cpp

---
+++ Stacks/PreviousGreater.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    vector<int> preGreaterEle(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> ans(n,-1);
+        stack<int> st;
+        for(int i=0;i<n;i++){
+            while(st.size() > 0 && st.top() <= arr[i]) 
+                st.pop();
+            if(st.size() == 0) ans[i] = -1;
+            else ans[i] = st.top();
+            st.push(arr[i]);
+        }
+        return ans;
+    }
+};

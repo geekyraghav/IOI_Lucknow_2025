957c920ae6350a721e3c65f6bffc0e5622c05aa1
geekyraghav <49072935+geekyraghav@users.noreply.github.com>
2026-09-08 10:14:34 +0530

    Create Preorder.cpp

---
+++ Binary_Trees/Preorder.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void dfs(TreeNode* root, vector<int>& ans) {
+        if(root == NULL) return;
+        ans.push_back(root->val);
+        dfs(root->left,ans);
+        dfs(root->right,ans);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        dfs(root,ans);
+        return ans;
+    }
+};
